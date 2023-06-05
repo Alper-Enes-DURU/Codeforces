f3093bdbca28087f34239a3610f77b30cdc909dd
Alper DURU <enesduru@gmail.com>
2023-06-04 20:10:22 -0500

    Word Capitalization

---
+++ 281A.cpp
@@ -0,0 +1,23 @@
+/**
+ * Link: https://codeforces.com/problemset/problem/281/A
+ * @author Alper Duru
+ */
+#include <iostream>
+#include <cstring>
+#include <math.h>
+#include <cstdio>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <map>
+using namespace std;
+
+int main()
+{
+    string s;
+    cin >> s;
+
+    s[0] = toupper(s[0]);
+    cout << s;
+    return 0;
+}
